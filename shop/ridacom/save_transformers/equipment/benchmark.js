35aa12cd16ddcaf6d7c0dbdc5c78b4da05df2af4
panagonov <panagonov@gmail.com>
2019-04-30 15:51:09 +0300

    fix "benchmark" and "accuris" products

--- shop/ridacom/save_transformers/equipment/benchmark.js
+++ shop/ridacom/save_transformers/equipment/benchmark.js
@@ -13,10 +13,10 @@ let product_props = {};
 
 main_product_id_fixator_list = main_product_id_fixator_list.reduce((res, item) => {
     if (!/\-E$/.test(item))
-        res.push(item + "-E");
-    res.push(item);
+        res[item + "-E"] =1;
+    res[item] = 1;
     return res
-}, []);
+}, {});
 
 let read_props = async (files) =>
 {
@@ -46,118 +46,112 @@ read_props([`${__dirname}/benchmark/props.csv`, `${__dirname}/benchmark/enzyme_p
 
 let get_real_oid = oid =>
 {
-    return id_fixes_map[oid] || oid.replace(/\-E$/, "")
+    return id_fixes_map[oid] || oid.replace(/\*?([-|~]E)?\*?$/,"")
 };
 
+let _check_is_main_product =  record => !!(main_product_id_fixator_list[record.oid] || record.crawler_item[get_real_oid(record.oid)]);
+
+let _get_main_product =  record => (record.crawler_item.table_specification || []).filter(item => record.crawler_item[get_real_oid(item.oid)])[0];
+
+
 let load_crawler_data = async(items, crawler_db) => {
-    let crawler_ids = items.reduce((res, {oid}) => {
-        if (id_fixes_map[oid])
-        {
-            res.push(id_fixes_map[oid]);
-        }
-        else if (/\-E$/.test(oid))
-        {
-            res.push(oid, oid.replace(/\-E$/, ""))
-        }
-        else {
-            res.push(oid)
-        }
-        return res;
+    let get_id_variations = oid =>{
+        return utils.uniq( [oid, id_fixes_map[oid], oid.replace("-", "~")]
+            .filter(item => item)
+            .reduce((r, it) =>{
+                let clean_oid = it.replace(/\*?([-|~]E)?\*?$/,"");
+                r.push(it, clean_oid, clean_oid +"*", clean_oid +"-E", clean_oid + "*-E", clean_oid + "-E*", clean_oid +"~E", clean_oid + "*~E", clean_oid + "~E*");
+                return r
+            }, [])
+        );}
+
+
+    let ids = items.reduce((res,item) => {
+        let new_ids = get_id_variations(item.oid);
+        return res.concat(new_ids)
     }, []);
-    let crawler_data = await crawler_db.read(collection_name, {body: {oid: {$in : crawler_ids}}});
 
-    let result = crawler_data.reduce((res, item) =>
-    {
-        if (typeof item.oid === "string"){
-            res[item.oid] = item;
-        }
-        else
-        {
-            item.oid.forEach(oid => res[oid] = item)
-        }
-        return res;
+    let crawler_data = await crawler_db.read(collection_name, {body: {"src" : "benchmark", "table_specification.oid" : {$in : ids}}});
 
-    }, {});
+    let hash = {};
+    crawler_data.forEach(item => {
+        (item.table_specification || []).forEach(res => {
+            let all_oids = get_id_variations(res.oid);
+            all_oids.forEach(oid => {
+                hash[oid] = Object.assign(item, res);
+            })
+        });
+    });
 
-    return result
+    let crawler_data_by_name = await crawler_db.read(collection_name, {body: {"src" : "benchmark", "table_specification.name" : {$in : ids}}});
+
+    crawler_data_by_name.forEach(item => {
+        (item.table_specification || []).forEach(res => {
+            hash[res.name] = Object.assign(item, res);
+        });
+    });
+
+    return hash
 };
 
 let _getProductRelations = (record) => {
     let oid = get_real_oid(record.oid);
-    if (record.supplies) {
-        return record.supplies.related.reduce((res, item) => {
-            let related = [];
-            if (item.table_specification && item.table_specification.length) {
-                related = item.table_specification.filter(item => item.oid !== oid).map(({oid}) => `PRODUCT_SOURCE:[BENCHMARK]_SUPPLIER:[RIDACOM]_ID:[${oid}]`);
-            }
-            return res.concat(related);
-        }, []);
-    } else if (record.crawler_item) {
+    if (record.crawler_item) {
         let res = [];
 
         if (record.crawler_item[id_fixes_map[record.oid] || record.oid])
             oid = id_fixes_map[record.oid] || record.oid;
 
-        let relations = record.crawler_item[oid];
-        if (relations) {
-            (record.crawler_item[oid].related).forEach(item => {
-                if (item.oid) {
-                    let related = typeof item.oid === "string" ? [item.oid] : item.oid;
-                    related.forEach(oid => res.push(`PRODUCT_SOURCE:[BENCHMARK]_SUPPLIER:[RIDACOM]_ID:[${oid}]`));
-                }
-
-            });
+        if(record.crawler_item.table_specification) {
+            res = record.crawler_item.table_specification.filter(item => item.oid).reduce((res, item) => {
+                if (item.oid !== oid)
+                    res.push(`PRODUCT_SOURCE:[BENCHMARK]_SUPPLIER:[RIDACOM]_ID:[${get_real_oid(item.oid)}]`);
+                return res
+            }, []);
         }
         if (res.length)
             return res;
     }
+    return null;
 };
 
 let _get_product_category = record => {
-    if (record.supplies)
-        return import_utils.get_canonical("Accessories", ":product_category");
+    let is_main_product = _check_is_main_product(record);
+    let result = [];
 
-    let result = import_utils.get_canonical(record.name.replace("™", " "), ":product_category");
+    if (is_main_product){
+        result = import_utils.get_canonical(record.name.replace("™", " "), ":product_category");
 
-    if (!result.length && !utils.isEmptyObj(record.crawler_item)){
-        result = import_utils.get_canonical((record.crawler_item.name).replace("™", " ") || "", ":product_category");
+        if (!result.length && !utils.isEmptyObj(record.crawler_item)){
+            result = import_utils.get_canonical((record.crawler_item.name || record.name).replace("™", " ") || "", ":product_category");
+        }
     }
-    else if(!result.length){
-        result = import_utils.get_canonical("Accessories", ":product_category");
-    }
 
-    return result
+    return result.length ? result :import_utils.get_canonical("Accessories", ":product_category");
 };
 
 let _get_product_sub_category = record => {
-    let category = _get_product_category(record);
-    if (record.supplies && record.supplies.related && record.supplies.related.length)
-    {
-        for (let i = 0; i < record.supplies.related.length; i++)
-        {
-            let res = import_utils.get_canonical(record.supplies.related[i].name.replace("™", " "), ":product_sub_category");
-            if (!res.length && category[0] && category[0][1] === "accessory")
-                res = import_utils.get_canonical(record.supplies.related[i].name.replace("™", " "), ":product_category");
+    let is_main_product = _check_is_main_product(record);
 
-            if (res.length)
-                return res
+    let result = import_utils.get_canonical(record.name.replace("™", " "), ":product_sub_category");
+    if (!result.length)
+        result = import_utils.get_canonical((record.crawler_item.name || "").replace("™", " "), ":product_sub_category");
+    if (!result.length && !is_main_product){
+        let main_product = _get_main_product(record);
+        if (main_product) {
+            result = import_utils.get_canonical((main_product.name || "").replace("™", " "), ":product_category");
+            if (!result.length)
+                result = import_utils.get_canonical((main_product.name || "").replace("™", " "), ":product_sub_category");
         }
-        return [];
     }
-    else {
-
-        let result = import_utils.get_canonical(record.name.replace("™", " "), ":product_sub_category");
-        if (!result.length)
-            result = import_utils.get_canonical((record.crawler_item.name || "").replace("™", " "), ":product_sub_category");
-        return result
-    }
+    return result
 };
 
 let _get_images = record =>
 {
-    if (record.supplies && record.supplies.related && record.supplies.related[0] && record.supplies.related[0].product_relations) {
-        for (let i = 0; i < record.supplies.related[0].product_relations.length; i++){
-            let relation = record.supplies.related[0].product_relations[i];
+    if (record.crawler_item.product_relations) {
+        for (let i = 0; i < record.crawler_item.product_relations.length; i++){
+            let relation = record.crawler_item.product_relations[i];
             let oid = relation.oid.split("\n").shift().trim();
 
             if (oid.indexOf(" with ") !== -1)
@@ -166,9 +160,9 @@ let _get_images = record =>
             if (oid === record.oid && relation.image )
                 return [{link: relation.image}]
         }
-        return record.supplies.related[0].images
     }
-    else if (record.crawler_item.images)
+
+    if (record.crawler_item.images)
     {
         return record.crawler_item.images
     }
@@ -177,14 +171,7 @@ let _get_images = record =>
 
 let _get_original_link = record =>
 {
-    if(record.crawler_item.link){
-        return record.crawler_item.link
-    }
-    else if(record.supplies && record.supplies.related && record.supplies.related.length){
-        return record.supplies.related[0].link
-    }
-
-    return null
+    return record.crawler_item.link
 };
 
 
@@ -196,7 +183,11 @@ let mapping = {
     "price_model"         : "price",
     "supplier"            : record => {
         let original_link = _get_original_link(record);
-        let supplier = !original_link || /benchmarkscientific/.test(original_link) ? "Benchmark Scientific" : "Accuris Instruments";
+        let supplier ="Benchmark Scientific";
+        if (original_link && /accuris-usa/.test(original_link))
+        {
+            supplier += " Accuris"
+        }
         return import_utils.get_canonical(supplier, ":supplier");
     },
     "distributor"         : () => import_utils.get_canonical("RIDACOM Ltd.", ":distributor"),
@@ -206,23 +197,23 @@ let mapping = {
     "product_relations"   : _getProductRelations,
     "images"              : _get_images,
     "pdf"                 : "crawler_item.pdf",
-    "original_link"       : _get_original_link
+    "original_link"       : "crawler_item.link"
 };
 
 let index = 0;
-let stop_after = -1;
+let stop_after = 400;
 let crawler_not_found = [];
 let show_in_console = (result, crawler_item, record) =>
 {
     if (index >= stop_after) {
-        let crawler_found = !utils.isEmptyObj(crawler_item) || !!record.supplies;
+        let crawler_found = !utils.isEmptyObj(crawler_item);
         console.table({
             index         : index,
             name          : result.name,
             oid           : result.oid,
-            // category      : (result.category || []).toString(),
-            // sub_category  : (result.sub_category || []).toString(),
-            // all_categories: (result.all_categories || []).toString(),
+            category      : (result.category || []).toString(),
+            sub_category  : (result.sub_category || []).toString(),
+            all_categories: (result.all_categories || []).toString(),
             supplier      : result.supplier[0],
             crawler       : crawler_found
         });
@@ -236,7 +227,7 @@ let show_in_console = (result, crawler_item, record) =>
     index++;
 };
 
-let convert = (item, crawler_item, custom_data) =>
+let convert = (item, crawler_item) =>
 {
     // if (stop_after >= 0 && index < stop_after) {        //todo only in test mode
     //     index++;
@@ -244,19 +235,12 @@ let convert = (item, crawler_item, custom_data) =>
     // }
 
     let missing_data =  [];
-    let supplies = custom_data[id_fixes_map[item.oid] || item.oid];
 
-    if (supplies && main_product_id_fixator_list.indexOf(item.oid) !== -1){
-        crawler_item = supplies.related[0];
-        crawler_item[item.oid] = {related: []};
-        supplies = null
-    }
-
-    let record = Object.assign({}, item, {crawler_item: crawler_item || {}, supplies : supplies});
+    let record = Object.assign({}, item, {crawler_item: crawler_item || {}});
 
     let result = utils.mapping_transform(mapping, record);
 
-    if (product_props[result.oid]){
+    if (product_props[result.oid]) {
         result = Object.assign(result, product_props_parser(product_props[result.oid], item.src, item.tid));
         if(result.size && result.size.length) {
             result.size.forEach((item, index) => {
@@ -283,79 +267,16 @@ let convert = (item, crawler_item, custom_data) =>
     }
 };
 
-let load_custom_data = async(mongo_db, crawler_db, result) => {
-
-    let ids = result
-        .reduce((res,item) =>{
-            res.push(item.oid);
-            res.push(id_fixes_map[item.oid]);
-            res.push(item.oid.replace("-", "~"));
-            res.push(item.oid.replace("-", "~") +"-E");
-            res.push(item.oid + "-E");
-            res.push(item.oid.replace(/\-E$/, ""));
-            res.push(item.oid.replace(/\-E$/, "").replace("-", "~"));
-            res.push(item.oid.replace(/\*$/, ""));
-            res.push(item.oid.replace(/\*$/, "").replace("-", "~"));
-            res.push(item.oid.replace(/\*$/, "-E"));
-            return res
-        }, [])
-        .filter(id => id);
-
-    ids = utils.uniq(ids);
-
-    let crawler_data = await crawler_db.read("product", {body: {"src" : "benchmark", "table_specification.oid" : {$in : ids}}});
-
-    let hash = {};
-    crawler_data.forEach(item => {
-        (item.table_specification || []).forEach(res => {
-            let all_oids = utils.uniq([
-                res.oid,
-                id_fixes_map[res.oid],
-                res.oid.replace("~", "-"),
-                res.oid.replace("~", "-") + "-E",
-                res.oid + "-E",
-                res.oid.replace(/\-E$/, ""),
-                res.oid.replace(/\-E$/, "").replace("~", "-"),
-                res.oid.replace(/\*$/, ""),
-                res.oid.replace(/\*$/, "").replace("~", "-"),
-                res.oid.replace(/\*$/, "-E")
-            ]).filter(item => item);
-            all_oids.forEach(oid => {
-                hash[oid] = hash[oid] || {};
-                hash[oid].item = res;
-                hash[oid].related = hash[oid].related || [];
-                hash[oid].related.push(item)
-            })
-        });
-    });
-
-    let crawler_data_by_name = await crawler_db.read("product", {body: {"src" : "benchmark", "table_specification.name" : {$in : ids}}});
-
-    crawler_data_by_name.forEach(item => {
-        (item.table_specification || []).forEach(res => {
-            hash[res.name] = hash[res.name] || {};
-            hash[res.name].item = res;
-            hash[res.name].related = hash[res.name].related || [];
-            hash[res.name].related.push(item)
-        });
-    });
-
-
-    return {result: hash};
-};
-
 let get_crawler_item = (item, crawler_hash) =>  {
     let oid = get_real_oid(item.oid);
     return crawler_hash[oid] || crawler_hash[item.oid];
 };
 
-
 module.exports = {
     convert,
     load_crawler_data,
-    load_custom_data,
-    get_crawler_item,
-    version: 22,
+    version: 25,
+    get_crawler_item
     // disable: true
 };
 
